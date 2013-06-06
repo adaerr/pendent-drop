ba8b44841f3d80009ecce096f6790409305deb75
Adrian Daerr <adrian.daerr@univ-paris-diderot.fr>
2013-06-06 15:33:18 +0200

    Create jar file only if compilation succeeds

--- makejar.sh
+++ makejar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/zsh
-fiji --javac *.java
+fiji --javac *.java &&\
 jar cvfM Goutte_pendante.jar Goutte_pendante{*.class,.java,.pdf,.tex} dropsketch.pdf PlugInDoc.{class,java} plugins.config
